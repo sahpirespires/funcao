8c68de733f008942e48c326ce0c15e8efd663c91
sahpirespires <124930619+sahpirespires@users.noreply.github.com>
2023-06-30 10:14:43 -0300

    Update ex3.py

--- funções/ex3.py
+++ funções/ex3.py
@@ -1,10 +1,5 @@
-def maior(*valores):
-    maior_valor = None
-    for valor in valores:
-        if maior_valor is None or valor > maior_valor:
-            maior_valor = valor
-    return maior_valor
+def maior(*num):
+    c = max(num)
+    print("O número maior é: ",c)
 
-
-resultado = maior(10, 5, 8, 15, 3)
-print("O maior valor é:", resultado)
+maior(30,1111,22,4957)
